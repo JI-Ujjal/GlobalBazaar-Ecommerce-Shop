7000d29fad24b175a61f01b25ae12ccb8be3ec35
JI-Ujjal <jiujjal85@gmail.com>
2023-04-29 11:51:27 +0600

    others

--- resources/views/frontend/headermenu/pages/shopDetails.blade.php
+++ resources/views/frontend/headermenu/pages/shopDetails.blade.php
@@ -10,12 +10,12 @@
                     <div class="product__details__pic__item">
                         <img class="product__details__pic__item--large" src="{{url('uploads/product', $Product->product_image)}}" alt="">
                     </div>
-                    <div class="product__details__pic__slider owl-carousel">
+                    <!-- <div class="product__details__pic__slider owl-carousel">
                         <img data-imgbigurl="{{url('frontend/assets/img/product/details/product-details-2.jpg')}}" src="{{url('frontend/assets/img/product/details/thumb-1.jpg')}}" alt="">
                         <img data-imgbigurl="{{url('frontend/assets/img/product/details/product-details-3.jpg')}}" src="{{url('frontend/assets/img/product/details/thumb-2.jpg')}}" alt="">
                         <img data-imgbigurl="{{url('frontend/assets/img/product/details/product-details-5.jpg')}}" src="{{url('frontend/assets/img/product/details/thumb-3.jpg')}}" alt="">
                         <img data-imgbigurl="{{url('frontend/assets/img/product/details/product-details-4.jpg')}}" src="{{url('frontend/assets/img/product/details/thumb-4.jpg')}}" alt="">
-                    </div>
+                    </div> -->
                 </div>
             </div>
             <div class="col-lg-6 col-md-6">
@@ -37,18 +37,18 @@
                     <!-- <a href="#" class="heart-icon"><span class="icon_heart_alt"></span></a> -->
                     <ul>
                         <li><b>Availability</b> <span style="color: red;">In Stock : {{$Product->product_quantity}} (kg/Pis)</span></li>
-                        <li><b>Share on</b>
+                        <!-- <li><b>Share on</b>
                             <div class="share">
                                 <a href="#"><i class="fa fa-facebook"></i></a>
                                 <a href="#"><i class="fa fa-twitter"></i></a>
                                 <a href="#"><i class="fa fa-instagram"></i></a>
                                 <a href="#"><i class="fa fa-pinterest"></i></a>
                             </div>
-                        </li>
+                        </li> -->
                     </ul>
                 </div>
             </div>
-            <div class="col-lg-12">
+            <!-- <div class="col-lg-12">
                 <div class="product__details__tab">
                     <ul class="nav nav-tabs" role="tablist">
                         <li class="nav-item">
@@ -124,7 +124,7 @@
                         </div>
                     </div>
                 </div>
-            </div>
+            </div> -->
         </div>
     </div>
 </section>
